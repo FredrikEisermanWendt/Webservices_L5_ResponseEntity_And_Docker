fb5b1669b152e12929e7a2baf817c28a0f9ed18f
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2024-08-29 09:55:22 +0200

    initial commit

---
+++ ws_lession_5_response_entity_and_docker/src/main/java/com/fredrik/ws_lession_5_response_entity_and_docker/WsLession5ResponseEntityAndDockerApplication.java
@@ -0,0 +1,13 @@
+package com.fredrik.ws_lession_5_response_entity_and_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsLession5ResponseEntityAndDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsLession5ResponseEntityAndDockerApplication.class, args);
+	}
+
+}
---
+++ ws_lession_5_response_entity_and_docker/src/test/java/com/fredrik/ws_lession_5_response_entity_and_docker/WsLession5ResponseEntityAndDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredrik.ws_lession_5_response_entity_and_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsLession5ResponseEntityAndDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
